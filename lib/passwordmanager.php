c2c723458bbd143bed967f4f1d63f25676fe7f0f
Jan Martijn van der Werf <janmartijn@vdwerf.eu>
2020-03-26 15:23:17 +0100

    Use of __DIR__ to require files

--- lib/passwordmanager.php
+++ lib/passwordmanager.php
@@ -1,8 +1,8 @@
 <?php
-require_once('PHPMailer/PHPMailer.php');
-require_once('PHPMailer/Exception.php');
+require_once(__DIR__.'/PHPMailer/PHPMailer.php');
+require_once(__DIR__.'/PHPMailer/Exception.php');
 
-require_once('../config.php');
+require_once(__DIR__.'/../config.php');
 
 function generateRandomString(
     $length = 64,
