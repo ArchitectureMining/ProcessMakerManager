c888822c4eebc5614c8bbb631056760ec52ccc62
Jan Martijn van der Werf <janmartijn@vdwerf.eu>
2020-03-26 15:56:26 +0100

    Added first scripts for management

---
+++ bin/createworkspace.php
@@ -0,0 +1,58 @@
+<?php
+
+if ($_SERVER['argc'] < 1) {
+	echo 'Usage of this script: ./'.$_SERVER['argv'][0].' <workspace id>'.PHP_EOL;
+	echo '  workspace id should be at most 13 characters long'.PHP_EOL;
+	echo '  (c) 2020, Jan Martijn van der Werf, Utrecht University'.PHP_EOL;
+
+	exit 1;
+}
+
+if (strlen($_SERVER['argv'][1]) > 13) {
+	die('workspace id is too long. I expected at most 13 characters. Instead I got: '.strlen($_SERVER['argv'][1]).' characters'.PHP_EOL);
+}
+
+require_once(__DIR__'/../config.php');
+
+
+if (!isset($workspace_template) || !is_file($workspace_template)) {
+	die('Variable $workspace_template does not point to a file in config.php!');
+}
+
+if (!isset($processmaker_cmd) || !is_file($processmaker_cmd)) {
+	die('Variable $processmaker_cmd does not point to a file in config.php!');
+}
+
+if (!isset($php_cmd) || !is_file($php_cmd)) {
+	die('Variable $php_cmd does not point to a file in config.php!');
+}
+
+$database = 'wf_'.$_SERVER['argv'][1];
+
+$con = mysqli_connect($db_host, $db_user, $db_pass);
+if (mysqli_connect_errno()) {
+  die('Database error: ' . mysqli_connect_error());
+}
+
+$stmt = $con->prepare('SHOW DATABASES LIKE ?');
+$stmt->bind_param('s', $database);
+$stmt->execute();
+$stmt->store_result();
+
+if ($stmt->num_rows > 0) {
+	die('Instance "'.$_SERVER['argv'][1].'" already exists!');
+}
+
+$command = $php_cmd . ' ' . $processmaker_cmd . ' workspace-restore' . $workspace_template . ' ' . $_SERVER['argv'][1];
+
+$output = '';
+$return = 0;
+
+exec( $cmd, $output, $return);
+
+if ($return != 0) {
+	echo $cmd;
+	echo PHP_EOL . PHP_EOL . 'gave output: ' . PHP_EOL . PHP_EOL;
+	echo $output;
+	exit($return);
+}
---
+++ bin/resetpassword.php
@@ -0,0 +1,42 @@
+<?php
+
+if ($_SERVER['argc'] < 2) {
+	echo 'Usage of this script: ./'.$_SERVER['argv'][0].' <workspace id> <password>'.PHP_EOL;
+	echo '  workspace id should be at most 13 characters long'.PHP_EOL;
+	echo '  (c) 2020, Jan Martijn van der Werf, Utrecht University'.PHP_EOL;
+
+	exit 1;
+}
+
+if (strlen($_SERVER['argv'][1]) > 13) {
+	die('workspace id is too long. I expected at most 13 characters. Instead I got: '.strlen($_SERVER['argv'][1]).' characters'.PHP_EOL);
+}
+
+
+require_once(__DIR__'/../config.php');
+
+$database = 'wf_'.$_SERVER['argv'][1];
+$password = $_SERVER['argv'][2];
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $database);
+if (mysqli_connect_errno()) {
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+$stmtUsers = $con->prepare("UPDATE USERS SET USR_PASSWORD=md5(?) WHERE USR_UID='00000000000000000000000000000001';");
+$stmtUsers->bind_param('s', $password);
+$result = $stmtUsers->execute();
+if (!$result) {
+	die('Failed updating USERS table: ' . $stmtUsers->error);
+}
+$stmtUsers->close();
+
+$stmtRbac = $con->prepare("UPDATE RBAC_USERS SET USR_PASSWORD=md5(?) WHERE USR_UID='00000000000000000000000000000001';");
+$stmtRbac->bind_param('s', $password);
+$result = $stmtRbac->execute();
+if (!$result) {
+	die('Failed updating RBAC_USERS table: ' . $stmtRbac->error);
+}
+
+$stmtRbac->close();
+$con->close();
