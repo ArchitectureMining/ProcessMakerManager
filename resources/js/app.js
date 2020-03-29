b0ea81a8b4c2059bebf6a2ca3c75772eafdfcc94
Mitchell Klijs <mitchell1998@gmail.com>
2020-03-29 13:49:20 +0200

    Prepare for validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,15 @@
 import 'bootstrap';
 
-(function () {
-});
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+import * as validators from "vuelidate/lib/validators";
+
+import VuelidateErrorExtractor, { templates } from 'vuelidate-error-extractor'
+
+Vue.use(Vuelidate);
+Vue.use(VuelidateErrorExtractor, {
+  template: templates.singleErrorExtractor.bootstrap4
+})
+
+window.Vue = Vue;
+window.validators = validators;
