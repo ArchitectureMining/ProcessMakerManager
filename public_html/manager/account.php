2b80343d50dddbc9325532e7c2f09f448b03fbc3
Jan Martijn van der Werf <janmartijn@vdwerf.eu>
2020-03-29 23:46:14 +0200

    Title of pages

--- public_html/manager/account.php
+++ public_html/manager/account.php
@@ -79,7 +79,7 @@ $con->close();
 ?><!doctype html>
 <html lang="en">
 	<head>
-		<title>ProcessMaker Manager - Login</title>
+		<title>ProcessMaker Manager - Account</title>
 		<meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="css/app.css">
